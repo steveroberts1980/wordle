e817cf1be723afb16a0eff9bf13d76a281f24a14
sroberts <steveroberts1980@gmail.com>
2022-08-19 08:02:26 -0400

    Latest after a clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { flatMap } from 'rxjs';
+
+export class Letter {
+  letter: string = '';
+  state: number = 0;
+  background: string = 'gray';
+  id: number = -1;
+
+  constructor(id: number) {
+    this.id = id;
+  }
+
+  getBackground(): string {
+    switch (this.state) {
+      case 0:
+        return "gray";
+      case 1:
+        return "yellow";
+      case 2:
+        return "green";
+    }
+
+    return "gray";
+  }
+
+  updateState() {
+    this.state = (this.state + 1) % 3;
+    this.background = this.getBackground();
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +38,134 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wordle';
+
+  displayedColumns: string[] = ["0", "1", "2", "3", "4"];
+
+  wordleData: Letter[][];
+  private wordList: string[];
+  possibleMatches: string[];
+
+  constructor(private http: HttpClient) {
+    this.wordleData = [];
+    this.wordList = [];
+    this.possibleMatches = [];
+
+    this.loadWordList();
+    this.clear();
+  }
+
+  focusNext(id: number) {
+    document.getElementById((id+1).toString())?.focus();
+  }
+
+  loadWordList() {
+    console.log('loading word list');
+    this.http.get('assets/wordle.txt', {responseType: 'text'}).subscribe(data => {
+      const list = data.split('\n');
+
+      list.forEach(w => {
+        this.wordList.push(w);
+      });
+    });
+  }
+
+  findWords() {
+    this.possibleMatches = [];
+    var badLetters: string = '';
+    var goodLetters: string = '';
+    var knownPositionLetters: string = '*****';
+
+    for (var i: number = 0; i < 6; i++) {
+      for (var j: number = 0; j < 5; j++) {
+        const w: Letter = this.wordleData[i][j];
+
+        if (w.letter != '') {
+          w.letter = w.letter.toLowerCase();
+
+          // w.state == 0 is NOT IN THE WORD
+          if (w.state == 0) {
+            badLetters += w.letter;
+          } else if (w.state == 1) {
+            // Right letter, wrong place.
+            goodLetters += w.letter;
+          } else {
+            // Right letter, right place.
+            goodLetters += w.letter;
+
+            var n = "";
+
+            for (var k: number = 0; k < 5; k++) {
+              if (k==j) {
+                n+= w.letter;
+              } else {
+                n += knownPositionLetters[k];
+              }
+            }
+
+            knownPositionLetters = n;
+          }
+        }
+      }
+    }
+
+    for (var i: number = 0; i < goodLetters.length; i++) {
+      badLetters = badLetters.replace(goodLetters[i], '');
+    }
+
+    for (var i: number = 0; i < 5; i++) {
+      badLetters = badLetters.replace(knownPositionLetters[i], '');
+    }
+
+    this.wordList.forEach(w => {
+      var isPossibleMatch: boolean = true;
+
+      // First remove any words that contain invalid letters
+      for (var i: number = 0; i < badLetters.length; i++) {
+        if (w.indexOf(badLetters[i]) > -1) {
+          isPossibleMatch = false;
+          break;
+        }
+      }
+
+      // Now remove any words that don't contain a valid letter
+      for (var i: number = 0; i < goodLetters.length; i++) {
+        if (w.indexOf(goodLetters[i]) == -1) {
+          isPossibleMatch = false;
+          break;
+        }
+      }
+
+      // Now remove any words that don't contain a valid letter in the right position
+      for (var i: number = 0; i < knownPositionLetters.length; i++) {
+        if (knownPositionLetters[i] != '*') {
+          if (w.charAt(i) != knownPositionLetters[i]) {
+            isPossibleMatch = false;
+
+            break;
+          }
+        }
+      }
+
+      if (isPossibleMatch) {
+        this.possibleMatches.push(w);
+      }
+    });
+  }
+
+  clear() {
+    this.wordleData = [];
+    this.possibleMatches = [];
+
+    for (var i: number = 0; i < 6; i++) {
+      this.wordleData[i] = [];
+      for (var j: number = 0; j < 5; j++) {
+        this.wordleData[i][j] = new Letter(i*5+j);
+      }
+    }
+  }
+
+  tabnext(e: any) {
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
